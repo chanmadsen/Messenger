b20e128f9f580fa69ea5ac343c570483f3ec07d2
Lon Madsen <braveshears@gmail.com>
2021-09-14 13:57:41 -0600

    Getting Started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Lon Chandler Madsen on 9/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
